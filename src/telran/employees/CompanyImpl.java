9209816a3c5618387abe78b92ffb334f50044f5b
Kulicod <fridman1986@gmail.com>
2023-03-18 08:13:39 +0200

    Company YG impl

--- src/telran/employees/CompanyImpl.java
+++ src/telran/employees/CompanyImpl.java
@@ -1,111 +1,229 @@
 package telran.employees;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
+import java.io.*;
 
 public class CompanyImpl implements Company {
 
-	HashMap<Long, Employee> hashMap = new HashMap<Long, Employee>();
-
+	
 	private static final long serialVersionUID = 1L;
-
+	private HashMap<Long, Employee> employees = new HashMap<>();
+	private HashMap<Integer, Set<Employee>> employeesMonth = new HashMap<>();
+	private HashMap<String, Set<Employee>> employeesDepartment = new HashMap<>();
+	private TreeMap<Integer, Set<Employee>> employeesSalary = new TreeMap<>();
+	
 	@Override
 	public Iterator<Employee> iterator() {
-		return hashMap.values().iterator();
+		
+		return getAllEmployees().iterator();
 	}
 
 	@Override
 	public boolean addEmployee(Employee empl) {
-		if (!hashMap.containsKey(empl.id)) {
-			hashMap.put(empl.id, empl);
-			return true;
+		boolean res = false;
+		if (employees.putIfAbsent(empl.id, empl) == null) {
+			res = true;
+			addIndexMap(employeesMonth, empl.getBirthDate().getMonthValue(), empl);
+			addIndexMap(employeesDepartment, empl.getDepartment(), empl);
+			addIndexMap(employeesSalary, empl.getSalary(), empl);
 		}
-		return false;
+		
+		return res;
+	}
+
+	private <T> void addIndexMap(Map<T, Set<Employee>> map, T key, Employee empl) {
+		map.computeIfAbsent(key, k->new HashSet<>()).add(empl);
+		
 	}
 
 	@Override
 	public Employee removeEmployee(long id) {
-		if (hashMap.containsKey(id)) {
-			Employee removedEmployee = hashMap.get(id);
-			hashMap.remove(id);
-			return removedEmployee;
+		Employee empl = employees.remove(id); 
+		if (empl != null) {
+			removeIndexMap(employeesMonth, empl.getBirthDate().getMonthValue(), empl);
+			removeIndexMap(employeesDepartment, empl.getDepartment(), empl);
+			removeIndexMap(employeesSalary, empl.getSalary(), empl);
 		}
-		return null;
+		return empl;
+	}
+
+	private <T>void removeIndexMap(Map<T, Set<Employee>> map, T key, Employee empl) {
+		Set<Employee> set = map.get(key);
+		set.remove(empl);
+		if (set.isEmpty()) {
+			map.remove(key);
+		}
+		
 	}
 
 	@Override
 	public List<Employee> getAllEmployees() {
-		return hashMap.values().stream().collect(Collectors.toList());
+		
+		return new ArrayList<>(employees.values());
 	}
 
 	@Override
 	public List<Employee> getEmployeesByMonthBirth(int month) {
-		return hashMap.values().stream().filter(empl -> empl.birthDate.getMonthValue() == month)
-				.collect(Collectors.toList());
-
+		
+		return new ArrayList<>(employeesMonth.getOrDefault(month, Collections.emptySet()));
 	}
 
 	@Override
 	public List<Employee> getEmployeesBySalary(int salaryFrom, int salaryTo) {
-		return hashMap.values().stream().filter(empl -> empl.salary > salaryFrom & empl.salary < salaryTo)
-				.collect(Collectors.toList());
+		
+		return employeesSalary.subMap(salaryFrom, true, salaryTo, true)
+				.values().stream().flatMap(Set::stream).toList();
 	}
 
 	@Override
 	public List<Employee> getEmployeesByDepartment(String department) {
-		return hashMap.values().stream().filter(empl -> empl.department == department).collect(Collectors.toList());
-
+		
+		return new ArrayList<>(employeesDepartment.getOrDefault(department, Collections.emptySet()));
 	}
 
 	@Override
 	public Employee getEmployee(long id) {
-		return hashMap.get(id);
+		
+		return employees.get(id);
 	}
 
 	@Override
 	public void save(String pathName) {
-		File file = new File(pathName);
-		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file))) {
-			for (Employee empl : hashMap.values()) {
-				output.writeObject(empl);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(pathName))){
+			output.writeObject(getAllEmployees());
+		} catch(Exception e) {
+			throw new RuntimeException(e.toString()); //some error
 		}
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void restore(String pathName) {
-		File file = new File(pathName);
-		boolean cont = true;
-		while (cont) {
-			try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
-				Employee obj = (Employee) input.readObject();
-				if (obj != null) {
-					hashMap.put(obj.id, obj);
-					
-				} else {
-					cont = false;
-				}
-				
-			} catch (Exception e) {
-			}
-hashMap.forEach((key, value) -> System.out.println(value));
+		try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(pathName))) {
+			List<Employee> allEmployees = (List<Employee>) input.readObject();
+			allEmployees.forEach(this::addEmployee);
+		}catch(FileNotFoundException e) {
+			//empty object but no error
+		} catch (Exception e) {
+			throw new RuntimeException(e.toString()); //some error
 		}
+
 	}
+
 }
+
+
+
+
+//package telran.employees;
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.ObjectInputStream;
+//import java.io.ObjectOutputStream;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//
+//public class CompanyImpl implements Company {
+//
+//	HashMap<Long, Employee> hashMap = new HashMap<Long, Employee>();
+//
+//	private static final long serialVersionUID = 1L;
+//
+//	@Override
+//	public Iterator<Employee> iterator() {
+//		return hashMap.values().iterator();
+//	}
+//
+//	@Override
+//	public boolean addEmployee(Employee empl) {
+//		if (!hashMap.containsKey(empl.id)) {
+//			hashMap.put(empl.id, empl);
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	@Override
+//	public Employee removeEmployee(long id) {
+//		if (hashMap.containsKey(id)) {
+//			Employee removedEmployee = hashMap.get(id);
+//			hashMap.remove(id);
+//			return removedEmployee;
+//		}
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Employee> getAllEmployees() {
+//		return hashMap.values().stream().collect(Collectors.toList());
+//	}
+//
+//	@Override
+//	public List<Employee> getEmployeesByMonthBirth(int month) {
+//		return hashMap.values().stream().filter(empl -> empl.birthDate.getMonthValue() == month)
+//				.collect(Collectors.toList());
+//
+//	}
+//
+//	@Override
+//	public List<Employee> getEmployeesBySalary(int salaryFrom, int salaryTo) {
+//		return hashMap.values().stream().filter(empl -> empl.salary > salaryFrom & empl.salary < salaryTo)
+//				.collect(Collectors.toList());
+//	}
+//
+//	@Override
+//	public List<Employee> getEmployeesByDepartment(String department) {
+//		return hashMap.values().stream().filter(empl -> empl.department == department).collect(Collectors.toList());
+//
+//	}
+//
+//	@Override
+//	public Employee getEmployee(long id) {
+//		return hashMap.get(id);
+//	}
+//
+//	@Override
+//	public void save(String pathName) {
+//		File file = new File(pathName);
+//		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file))) {
+//			for (Employee empl : hashMap.values()) {
+//				output.writeObject(empl);
+//			}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//	@Override
+//	public void restore(String pathName) {
+//		File file = new File(pathName);
+//		boolean cont = true;
+//		while (cont) {
+//			try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
+//				Employee obj = (Employee) input.readObject();
+//				if (obj != null) {
+//					hashMap.put(obj.id, obj);
+//					
+//				} else {
+//					cont = false;
+//				}
+//				
+//			} catch (Exception e) {
+//			}
+//hashMap.forEach((key, value) -> System.out.println(value));
+//		}
+//	}
+//}
--- src/telran/employees/CompanyTest.java
+++ src/telran/employees/CompanyTest.java
@@ -3,102 +3,223 @@ package telran.employees;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import telran.employees.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CompanyTest {
-	static String filePath = "company.data";
-	Employee empl1 = new Employee(1111, "vasya", LocalDate.parse("2007-12-03"), "front", 4000);
-	Employee empl2 = new Employee(2222, "petya", LocalDate.parse("2000-12-03"), "front", 5000);
-	Employee empl3 = new Employee(3333, "dima", LocalDate.parse("2000-06-03"), "back", 6000);
-	Employee empl4 = new Employee(4444, "tanya", LocalDate.parse("2000-06-03"), "back", 7000);
-	Employee empl5 = new Employee(5555, "lena", LocalDate.parse("2000-07-03"), "back", 8000);
-	Employee empl6 = new Employee(6666, "kostya", LocalDate.parse("2000-07-03"), "dev", 9000);
-
-	Company company = new CompanyImpl();
-
+private static final long ID1 = 123;
+private static final int MONTH1 = 1;
+private static final String DEPARTMENT1 = "department1";
+private static final int SALARY1 = 1000;
+private static final long ID2 = 124;
+private static final String DEPARTMENT2 = "department2";
+private static final int SALARY2 = 2000;
+private static final LocalDate BIRTH2 = LocalDate.of(2000, MONTH1, 1);
+private static final int MONTH2 = 2;
+private static final int SALARY3 = 3000;
+private static final long ID3 = 125;
+private static final int SALARY4 = 4000;
+private static final long ID4 = 126;
+private static final long ID10 = 100000;
+private static final String FILE_NAME = "test.data";
+Employee empl1 = new Employee(ID1, "name", LocalDate.of(2000, MONTH1, 1), DEPARTMENT1, SALARY1);
+Employee empl2 = new Employee(ID2, "name", LocalDate.of(2000, MONTH1, 1), DEPARTMENT2, SALARY2);
+Employee empl3 = new Employee(ID3, "name", LocalDate.of(2000, MONTH2, 1), DEPARTMENT1, SALARY3);
+Employee empl4 = new Employee(ID4, "name", LocalDate.of(2000, MONTH1, 1), DEPARTMENT2, SALARY4);
+Employee[] employees = {empl1, empl2, empl3, empl4};
+Company company;
 	@BeforeEach
-	void addTest() throws Exception {
-		assertTrue(company.addEmployee(empl1));
-		assertTrue(company.addEmployee(empl2));
-		assertTrue(company.addEmployee(empl3));
-		assertTrue(company.addEmployee(empl4));
-		assertTrue(company.addEmployee(empl5));
-		assertTrue(company.addEmployee(empl6));
-
-		assertFalse(company.addEmployee(empl1));
+	void setUp() throws Exception {
+		company = new CompanyImpl();
+		for(Employee empl: employees) {
+			company.addEmployee(empl);
+		}
 	}
 
 	@Test
-	void getEmployeeTest() {
-		assertEquals(empl1, company.getEmployee(1111));
-		assertNull(company.getEmployee(0000));
-	}
-
-	@Test
-	void byDepartmentTest() {
-		List<Employee> listByDepartment = company.getEmployeesByDepartment("back");
-		Object[] byDep = listByDepartment.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(byDep);
-		Object[] depExp = { (long) 3333, (long) 4444, (long) 5555 };
-		assertArrayEquals(depExp, byDep);
-	}
-
-	@Test
-	void byMonthTest() {
-		List<Employee> listByMonth = company.getEmployeesByMonthBirth(12);
-		Object[] byMon = listByMonth.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(byMon);
-		Object[] monExp = { (long) 1111, (long) 2222 };
-		assertArrayEquals(monExp, byMon);
-	}
-
-	@Test
-	void removeTest() {
-		assertEquals(empl3, company.removeEmployee(3333));
-		assertNull(company.removeEmployee(0000));
-
-		List<Employee> listByDepartment = company.getEmployeesByDepartment("back");
-		Object[] byDep = listByDepartment.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(byDep);
-		Object[] depExp = { (long) 4444, (long) 5555 };
-		assertArrayEquals(depExp, byDep);
-
-		List<Employee> listBySalary = company.getEmployeesBySalary(5500, 8500);
-		Object[] bySal = listBySalary.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(bySal);
-		Object[] salExp = { (long) 4444, (long) 5555 };
-		assertArrayEquals(bySal, salExp);
-	}
-
-	@Test
-	void getAllEmployeesTest() {
-		List<Employee> listAll = company.getAllEmployees();
-		Object[] all = listAll.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(all);
-		Object[] allExp = { (long) 1111, (long) 2222, (long) 3333, (long) 4444, (long) 5555, (long) 6666 };
-		assertArrayEquals(all, allExp);
-	}
-
-	@Test
-	void getEmployeesBySalaryTest() {
-		List<Employee> listBySalary = company.getEmployeesBySalary(5500, 8500);
-		Object[] bySal = listBySalary.stream().map(em -> em.getId()).toArray();
-		Arrays.sort(bySal);
-		Object[] salExp = { (long) 3333, (long) 4444, (long) 5555 };
-		assertArrayEquals(bySal, salExp);
+	void addEmployeeTest() {
+		Employee newEmployee = new Employee(ID10, DEPARTMENT2, BIRTH2, DEPARTMENT1, SALARY1);
+		assertTrue(company.addEmployee(newEmployee));
+		assertFalse(company.addEmployee(newEmployee));
 	}
 	@Test
+	void removeEmployeeTest() {
+		assertEquals(empl1, company.removeEmployee(ID1));
+		assertNull(company.removeEmployee(ID1));
+		runTest(new Employee[] {empl2, empl3, empl4});
+		
+	}
+	@Test
+	void employeesByMonthTest() {
+		assertTrue(company.getEmployeesByMonthBirth(20).isEmpty());
+		Employee[] expected = {empl1, empl2, empl4};
+		Employee[] actual = company.getEmployeesByMonthBirth(MONTH1).toArray(Employee[]::new);
+		Arrays.sort(actual);
+		assertArrayEquals(expected, actual);
+		company.removeEmployee(ID1);
+		company.removeEmployee(ID2);
+		company.removeEmployee(ID4);
+		assertTrue(company.getEmployeesByMonthBirth(MONTH1).isEmpty());
+	}
+	@Test
+	void employeesByDepartmentTest() {
+		assertTrue(company.getEmployeesByDepartment("gggggg").isEmpty());
+		Employee[] expected = {empl2, empl4};
+		Employee[] actual = company.getEmployeesByDepartment(DEPARTMENT2).toArray(Employee[]::new);
+		Arrays.sort(actual);
+		assertArrayEquals(expected, actual);
+		company.removeEmployee(ID2);
+		company.removeEmployee(ID4);
+		assertTrue(company.getEmployeesByDepartment(DEPARTMENT2).isEmpty());
+		
+	}
+	@Test
+	void employeesBySalaryTest() {
+		assertTrue(company.getEmployeesBySalary(100000000,100000100).isEmpty());
+		Employee[] expected = {empl1, empl2, empl3};
+		Employee[] actual = company.getEmployeesBySalary(SALARY1, SALARY3).toArray(Employee[]::new);
+		Arrays.sort(actual);
+		assertArrayEquals(expected, actual);
+		company.removeEmployee(ID1);
+		company.removeEmployee(ID2);
+		company.removeEmployee(ID3);
+		assertTrue(company.getEmployeesBySalary(SALARY1, SALARY3).isEmpty());
+	}
+
+	private void runTest(Employee[] expected) {
+		Employee[]actual = company.getAllEmployees().toArray(Employee[]::new);
+		Arrays.sort(actual);
+		assertArrayEquals(expected, actual);
+		
+	}
+	
+	@Test
+	@Order(1)
 	void saveTest() {
-		company.save(filePath);
+		company.save(FILE_NAME);
 	}
 	@Test
-	@Disabled
-	// не получается, идет зацикливание. Разбираюсь...
+	@Order(2)
 	void restoreTest() {
-				company.restore(filePath);
+		Company company2 = new CompanyImpl();
+		company2.restore(FILE_NAME);
+		assertIterableEquals(company, company2);
 	}
+	
+	
+
 }
+
+
+
+//package telran.employees;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import java.time.LocalDate;
+//import java.util.*;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//
+//class CompanyTest {
+//	static String filePath = "company.data";
+//	Employee empl1 = new Employee(1111, "vasya", LocalDate.parse("2007-12-03"), "front", 4000);
+//	Employee empl2 = new Employee(2222, "petya", LocalDate.parse("2000-12-03"), "front", 5000);
+//	Employee empl3 = new Employee(3333, "dima", LocalDate.parse("2000-06-03"), "back", 6000);
+//	Employee empl4 = new Employee(4444, "tanya", LocalDate.parse("2000-06-03"), "back", 7000);
+//	Employee empl5 = new Employee(5555, "lena", LocalDate.parse("2000-07-03"), "back", 8000);
+//	Employee empl6 = new Employee(6666, "kostya", LocalDate.parse("2000-07-03"), "dev", 9000);
+//
+//	Company company = new CompanyImpl();
+//
+//	@BeforeEach
+//	void addTest() throws Exception {
+//		assertTrue(company.addEmployee(empl1));
+//		assertTrue(company.addEmployee(empl2));
+//		assertTrue(company.addEmployee(empl3));
+//		assertTrue(company.addEmployee(empl4));
+//		assertTrue(company.addEmployee(empl5));
+//		assertTrue(company.addEmployee(empl6));
+//
+//		assertFalse(company.addEmployee(empl1));
+//	}
+//
+//	@Test
+//	void getEmployeeTest() {
+//		assertEquals(empl1, company.getEmployee(1111));
+//		assertNull(company.getEmployee(0000));
+//	}
+//
+//	@Test
+//	void byDepartmentTest() {
+//		List<Employee> listByDepartment = company.getEmployeesByDepartment("back");
+//		Object[] byDep = listByDepartment.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(byDep);
+//		Object[] depExp = { (long) 3333, (long) 4444, (long) 5555 };
+//		assertArrayEquals(depExp, byDep);
+//	}
+//
+//	@Test
+//	void byMonthTest() {
+//		List<Employee> listByMonth = company.getEmployeesByMonthBirth(12);
+//		Object[] byMon = listByMonth.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(byMon);
+//		Object[] monExp = { (long) 1111, (long) 2222 };
+//		assertArrayEquals(monExp, byMon);
+//	}
+//
+//	@Test
+//	void removeTest() {
+//		assertEquals(empl3, company.removeEmployee(3333));
+//		assertNull(company.removeEmployee(0000));
+//
+//		List<Employee> listByDepartment = company.getEmployeesByDepartment("back");
+//		Object[] byDep = listByDepartment.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(byDep);
+//		Object[] depExp = { (long) 4444, (long) 5555 };
+//		assertArrayEquals(depExp, byDep);
+//
+//		List<Employee> listBySalary = company.getEmployeesBySalary(5500, 8500);
+//		Object[] bySal = listBySalary.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(bySal);
+//		Object[] salExp = { (long) 4444, (long) 5555 };
+//		assertArrayEquals(bySal, salExp);
+//	}
+//
+//	@Test
+//	void getAllEmployeesTest() {
+//		List<Employee> listAll = company.getAllEmployees();
+//		Object[] all = listAll.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(all);
+//		Object[] allExp = { (long) 1111, (long) 2222, (long) 3333, (long) 4444, (long) 5555, (long) 6666 };
+//		assertArrayEquals(all, allExp);
+//	}
+//
+//	@Test
+//	void getEmployeesBySalaryTest() {
+//		List<Employee> listBySalary = company.getEmployeesBySalary(5500, 8500);
+//		Object[] bySal = listBySalary.stream().map(em -> em.getId()).toArray();
+//		Arrays.sort(bySal);
+//		Object[] salExp = { (long) 3333, (long) 4444, (long) 5555 };
+//		assertArrayEquals(bySal, salExp);
+//	}
+//	@Test
+//	void saveTest() {
+//		company.save(filePath);
+//	}
+//	@Test
+//	@Disabled
+//	// не получается, идет зацикливание. Разбираюсь...
+//	void restoreTest() {
+//				company.restore(filePath);
+//	}
+//}
